8b9035d4f729f4cddf7861496b74dc785cea9d03
Jeferson da Silva Araujo <jeff.araujo@gmail.com>
2018-09-17 22:50:24 -0300

    Add project files.

---
+++ UT.BoasPraticas/Internals/InternalsVisibleTesting.cs
@@ -0,0 +1,19 @@
+﻿using APP.AulaAtributo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UT.BoasPraticas.Internals
+{
+
+    [TestClass]
+    public class InternalsVisibleTesting
+    {
+        [TestMethod]
+        public void TestMethod_InternalsVisible()
+        {
+            AlunoDebugProxy alunoDebutProxy = new AlunoDebugProxy(new Aluno()); //AlunoDebugProxy visto graças ao AssemblyInfo do projeto
+        }
+    }
+}
